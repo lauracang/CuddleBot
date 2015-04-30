a7a07b18d7e6385a2eea0843b40709955f488a23
Laura Cang <laura.cang@gmail.com>
2015-04-29 18:13:06 -0700

    updating sensorreader

--- src/SensorReader.java
+++ src/SensorReader.java
@@ -32,7 +32,7 @@ public class SensorReader implements SerialPortEventListener {
 																				// X
 			"/dev/ttyACM0", // Raspberry Pi
 			"/dev/ttyUSB0", // Linux
-			"COM7", // Windows
+			"COM3", // Windows
 	};
 	/**
 	 * A BufferedReader which will be fed by a InputStreamReader converting the
