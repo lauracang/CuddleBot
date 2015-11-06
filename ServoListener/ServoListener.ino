8824b362d1840ea6949d203eda80355d0f34fc28
Laura Cang <laura.cang@gmail.com>
2015-11-06 15:02:08 -0800

    made serial write work

--- ServoListener/ServoListener.ino
+++ ServoListener/ServoListener.ino
@@ -8,7 +8,7 @@ const int servoPin = 9;      // the pin that the LED is attached to
 
 void setup() {
   // initialize the serial communication:
-  Serial.begin(9600);
+  Serial.begin(512000);
   // Attach the servoPin to the servo object
   myservo.attach(servoPin);
 
@@ -18,7 +18,7 @@ void loop() {
   byte pos;
   // check if data has been sent from the computer:
   if (Serial.available()) {
-    // read the most recent byte (which will be from 0 to 255):
+    // read the most recent byte:
     pos = Serial.read();
     // set the brightness of the LED:
     myservo.write((int) pos);
