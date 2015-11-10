e84769631e39445832899007bbbed8f4347ff632
pbucci <pbucci@gmail.com>
2015-11-09 22:17:55 -0800

    sine wave test

--- src/BitHandler.java
+++ src/BitHandler.java
@@ -1,9 +1,14 @@
+import java.util.concurrent.TimeUnit;
+
 
 public class BitHandler {
 	SerialWrite serial;
 	BufferQueue behaviours;
 	BitThread bitThread;
-	
+	double t = 0;
+	double hz = 0.01;
+	double amp = 90;
+	double offset = 90;
 	public BitHandler() {
 		
 		behaviours = new BufferQueue(20);
@@ -14,6 +19,13 @@ public class BitHandler {
 	public void update(String s) {
 		behaviours.push(s);
 	}
+	
+	
+	public int sine() {
+		double s = (amp * Math.sin(t*Math.PI)) + offset;
+		int i = (int) s;
+		return i;
+	}
 
 	private class BitThread implements Runnable {
 		int count = 0;
@@ -23,18 +35,31 @@ public class BitHandler {
 			serial.initialize();
 			serial.portConnect();
 			
-			loop(10000);
+			loop(-1);
 			
 			serial.close();
 			System.out.println("done sending fer now");
 		}
 		public void loop(int i) {
 			int j = 0;
+			if (i < 0) {
+				while(true) {
+					send(sine());
+//					 2 seconds per period
+					try {
+						TimeUnit.MILLISECONDS.sleep(20);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					t = t + hz;
+					System.out.println(sine());
+				}
+			}
 			while (j < i) {
 				j++;
 				send(180);
-			}
-			//TODO 
+			} 
 		}
 		public void send(short s) {
 			serial.write(s);
